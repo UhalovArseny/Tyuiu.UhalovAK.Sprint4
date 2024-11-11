fe5036245ebb0b2928db9ff440eae4b186d1ab29
Uhalov Arseny <akukhalov@gmail.com>
2024-11-11 18:33:17 +0500

    Task1

---
+++ Tyuiu.UhalovAK.Sprint4.Task1.V6.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.UhalovAK.Sprint4.Task1.V6.Lib
+{
+    public class DataService : ISprint4Task1V6
+    {
+        public int Calculate(int[] array)
+        {
+            int z = 1;
+            for (int i = 0; i <= array.Length - 1; i++)
+            {
+                if (array[i] % 2 == 0)
+                {
+                    z *= array[i];
+                }
+            }
+            return z;
+        }
+    }
+}
---
+++ Tyuiu.UhalovAK.Sprint4.Task1.V6/Program.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.UhalovAK.Sprint4.Task1.V6.Lib;
+namespace Tyuiu.UhalovAK.Sprint4.Task1.V6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* Спринт #4                                                                *");
+            Console.WriteLine("* Тема: Массивы                                                   *");
+            Console.WriteLine("* Задание #0                                                               *");
+            Console.WriteLine("* Вариант #4                                                               *");
+            Console.WriteLine("* Выполнил: Ухалов Арсений Константинович | АСОиУБ-24-1                    *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.WriteLine("****************************************************************************");
+
+            
+            DataService ds = new DataService();
+            int len;
+            len = Convert.ToInt32(Console.ReadLine());
+            int[] numArry = new int[len];
+
+            
+
+            for (int i = 0; i <= len - 1; i++)
+            {
+                numArry[i] = Convert.ToInt32(Console.ReadLine());
+            }
+                
+
+            
+
+            int z = ds.Calculate(numArry);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(z);
+            Console.ReadKey();
+        }
+    }
+}
